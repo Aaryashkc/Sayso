594f372da136b518f0f6a6fc1faaa27b22b4b6a9
Aaryashkc <aaryashkc123@gmail.com>
2025-05-13 09:18:13 +0545

    final final

--- frontend/src/components/EditProfileModal.jsx
+++ frontend/src/components/EditProfileModal.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
 import useUpdateProfile from "../hooks/useUpdateProfile";
 
 const EditProfileModal = ({authUser}) => {
