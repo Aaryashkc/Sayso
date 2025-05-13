3e5d68d8ab942dfc87033b2586189a59c7e94e41
Aaryashkc <aaryashkc123@gmail.com>
2025-05-13 08:16:06 +0545

    final final

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 
@@ -20,6 +21,7 @@ cloudinary.config({
 
 const app = express();
 const PORT= process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "10mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
@@ -31,6 +33,13 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationsRoutes);
+
+if(process.env.NODE_ENV !== 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get(/.*/, (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    })
+}
 
 
 
